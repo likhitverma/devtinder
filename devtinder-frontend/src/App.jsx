ba372a1f4101411ef64cd7d50f987e548d0e5f4e
Likhit Verma <likhit.vermacs@gmail.com>
2026-02-23 08:17:25 +0530

    Initialised react routing

--- devtinder-frontend/src/App.jsx
+++ devtinder-frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter basename={"/"}>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/home" element={<div>Home Page</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
